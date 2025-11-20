9ad020bda51b64d51a119b649d7c311f684a9c90
kouki20004 <oochi200453@gmail.com>
2025-11-20 16:20:50 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,3 +1,11 @@
-import streamit as st
+import streamlit as st
 
 st.title("明日")
+
+name = st.text_input("名前を入力")
+
+st.write(name)
+
+camera_phote = st.camera_input("写真を撮影してくれ")
+if camera:
+  st.image(camera, caption="写真",use_column_width=True)
