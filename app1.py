57147812f2c3cd0c5a021c0b4975d24de9be46ed
kouki20004 <oochi200453@gmail.com>
2025-11-27 15:20:43 +0900

    Add consent checkbox before camera input
    Added a checkbox for user consent before taking a photo.

--- app1.py
+++ app1.py
@@ -6,6 +6,9 @@ name = st.text_input("名前を入力")
 
 st.write(name)
 
-camera_phote = st.camera_input("写真を撮影してくれ")
+st.checkbox("同意します")
+
+
+camera = st.camera_input("写真を撮影してくれ")
 if    camera:
         st.image(camera, caption="写真",use_column_width=True)
