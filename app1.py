c57bdefcbeab0396fd41b3d27d4d8d1e7b0ed76c
kouki20004 <oochi200453@gmail.com>
2025-11-27 15:29:37 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -7,8 +7,10 @@ name = st.text_input("名前を入力")
 st.write(name)
 
 st.checkbox("同意します")
-st.selectbox("次の中から現住所を教えて",["京都府","大阪府"])
+address = st.selectbox("次の中から現住所を教えて",["京都府","大阪府"])
+st.write(address)
 
+st.multiselect("趣味を次から複数選んでください",["映画","音楽"])
 
 
 camera = st.camera_input("写真を撮影してくれ")
